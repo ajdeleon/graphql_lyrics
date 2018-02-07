7879e16f8d782ef91a2f2d744763c736e9a252f8
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-07 01:23:48 -0500

    add form to add lyrics to songs

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const mlab_key = require('./keys')
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = mlab_key;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
